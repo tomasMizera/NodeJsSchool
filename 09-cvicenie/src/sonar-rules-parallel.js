ccd7578b3f3fec710d126ec040b757db35655c4f
tomasMizera <tomas.mizera2@gmail.com>
2018-11-23 19:46:09 +0100

    DONE: bonus on write tmp file

--- 09-cvicenie/src/sonar-rules-parallel.js
+++ 09-cvicenie/src/sonar-rules-parallel.js
@@ -1,12 +1,37 @@
+
+
+// gets only third argument
 const [, , URL] = process.argv;
 
 const { parallelLimit } = require("async");
 const request = require("request")
-  .defaults({ json: true });
+			.defaults({ json: true });
 
 
 let pageIndex = 0;
 const results = [];
 
-parallelLimit(tasks, 4, callback
-	);
+parallelLimit(
+	(callback) => {
+		callback(err);
+	},
+
+	// limit of tasks at the time
+	4, 
+
+	// callback
+	function(err) {
+		if ( err ) throw err;
+		console.log( JSON.stringify( results, null, 2 ));
+	});
+
+// fs.writeFile(file, data[, options], callback)
+// replacing the file if it already exists. 
+// data can be a string or a buffer.
+// It is unsafe to use fs.writeFile() multiple times 
+// 	on the same file, without waiting for the callback
+
+fs.writeFile(outPath, outData, (err) => {
+    if (err) throw err;
+    console.error('written:', outPath);
+});
--- 09-cvicenie/src/writeTempFile1.js
+++ 09-cvicenie/src/writeTempFile1.js
@@ -6,33 +6,29 @@ const path = require("path");
 const async = require( 'async' );
 
 
-function writeTempFile(fileName, data, opts, cb /* data, options, callback*/ ) {
-	// let cb = args.pop();
-
+function writeTempFile(fileName, data, opts, cb ) {
 	let tempDir = path.join(os.tmpdir(), `${process.pid}-`);
 	
 	async.waterfall([
-			(callback) => {
-				fs.mkdtemp(tempDir, callback);
-				
-			},
-			(path, callback) => {
-				let filePath = path + '/' + fileName;
-				console.log(filePath);
-				try{
-					fs.writeFile( filePath, ...opts, (err) => {
-			    		if (err) throw err;
-			    		callback(null, filePath);
-			    	})
-				} catch(err) {
-					cb(err);
-				}
+		// Create a temp directory
+		(callback) => {
+			fs.mkdtemp(tempDir, callback);
+		},
+		// Write file to temp directory
+		(path, callback) => {
+			let filePath = path + '/' + fileName;	
+			try{
+				fs.writeFile( filePath, data, opts, (err) => {
+					if (err) throw err;
+					callback(null, filePath);
+				})
+			} catch(erro) {
+				callback(erro);
 			}
+		}
 		],
 		function(err, result) {
-			console.log("What ", result);
 			cb(err, result);
-		});
-
-  }
-  console.log(writeTempFile.length);
+		}
+	);
+}
